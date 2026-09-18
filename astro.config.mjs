149f98d293c95face1976c221715fd670bfdf956
Serein020 <weykeiii@163.com>
2026-09-18 17:47:36 +0800

    add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
+
 import vue from '@astrojs/vue';
+
 import cloudflare from '@astrojs/cloudflare';
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-  output: 'server',
+  output: isGitHubPages ? 'static' : 'server',
+
+  site: isGitHubPages
+    ? 'https://serein020.github.io'
+    : 'https://rocophotogallery.weykeiii.workers.dev',
+
+  base: isGitHubPages ? '/RocoPhotoGallery' : undefined,
+
   integrations: [vue()],
-  adapter: cloudflare(),
+
+  ...(isGitHubPages
+    ? {}
+    : {
+        adapter: cloudflare(),
+      }),
 });
