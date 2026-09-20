74053f432e6e0d9fab06ff610fba280789e2fa4a
Serein020 <weykeiii@163.com>
2026-09-21 06:13:47 +0800

    fix astro static build for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config';
-
 import vue from '@astrojs/vue';
 
-import cloudflare from '@astrojs/cloudflare';
-
-const isNetlify = Boolean(process.env.NETLIFY);
-
 export default defineConfig({
-  output: isNetlify ? 'static' : 'server',
+  output: 'static',
 
   integrations: [vue()],
-
-  ...(isNetlify
-    ? {}
-    : {
-        adapter: cloudflare(),
-      }),
 });
